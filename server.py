b8cdbccc3bf9c5c1fd8f8265b140235d17422b42
instant-results <72632415+instant-results@users.noreply.github.com>
2023-01-01 14:26:01 +0100

    Server side basic variable display with delay

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import sys
 from opcua import Server
+from random import randint
+import time
 
 server = Server()
 
@@ -15,6 +17,9 @@ objects = server.get_objects_node()
 # Populate the namespace
 myobj = objects.add_object(addspace, "MyObject")
 myvar = myobj.add_variable(addspace, "MyVariable", 6.7)
+myvar2 = myobj.add_variable(addspace, "MyVariable2", 123)
+#myvar is now writable on the client side
+myvar.set_writable()
 
 # Start the server
 server.start()
@@ -22,8 +27,14 @@ server.start()
 # Run the server forever
 try:
     while True:
-        pass
+        
+        myvar.set_value(randint(0, 20))
+        myvar2.set_value(randint(0, 20))
+        print(f"myvar = {myvar.get_value()}   myvar2 = {myvar2.get_value()}" )
+
+        time.sleep(2)
 except (KeyboardInterrupt, SystemExit):
     # Close the server
+    print("Server stop")
     server.stop()
     sys.exit()
